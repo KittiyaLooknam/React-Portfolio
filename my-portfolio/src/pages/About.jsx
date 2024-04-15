0c5af2b85c5b8ac738c987085df6f792055e8f77
KittiyaLooknam <kittiya.goldberg1@gmail.com>
2024-04-15 16:06:47 -0400

    ajust the text and the image.
    ```

--- my-portfolio/src/pages/About.jsx
+++ my-portfolio/src/pages/About.jsx
@@ -6,15 +6,15 @@ export default function About() {
     <div className="container-main">
       <div className="row">
         <div className="col-md-6 text-left"></div>
-          <h1 className="conten"style={{ color: 'white' }}>About me
-          </h1>
+        <h1 className="conten" style={{ color: 'white' }}>About me
+        </h1>
         <div className="row">
-          <div className="col-md-6"style={{ color: 'white' }}>
+          <div className=" col-md-6">
             <p>
               Welcome to my portfolio! My name is Kittiya Goldberg. I'm currently studying at the George Washington University coding bootcamp program in Full Stack Web Development. I have a degree in Fisheries Science and worked in Thailand for 2 years before moving to the USA as an aupair. Since being in the USA, I worked in child care until 2023. I became interested in web development after watching tutorials on YouTube.
             </p>
           </div>
-          <div className="col-md-6">
+          <div className="col-md-6 d-flex justify-content-center align-items-center">
             <img src={backgroundImage} alt="Aside" className="img-fluid" />
           </div>
         </div>
