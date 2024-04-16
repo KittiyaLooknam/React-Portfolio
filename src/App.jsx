4d9161119967ea3b063619f1c52baf3b6d93585e
KittiyaLooknam <kittiya.goldberg1@gmail.com>
2024-04-15 22:08:40 -0400

    change to Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
 import Footer from './pages/Footer';
