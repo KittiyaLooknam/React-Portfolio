9869138547aa234db04ae4d81257ad71ab3a79aa
KittiyaLooknam <kittiya.goldberg1@gmail.com>
2024-06-05 17:07:04 -0400

    changed the same id at the project id

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -6,6 +6,10 @@ import Project3Img from '../assets/logo.svg.png';
 import Project4Img from '../assets/Note-Taker.png';
 import Project5Img from '../assets/Password-Generator.png';
 import Project6Img from '../assets/workday-sceduler.png';
+import Project7Img from '../assets/Quiz-game-challenge.png';
+import Project8Img from '../assets/Social-Network-API.png';
+import Project9Img from '../assets/Employee-Tracker.png';
+
 import '../style.css'; // Import the CSS file
 import FancyText from '@carefully-coded/react-text-gradient';
 
@@ -54,6 +58,51 @@ export default function Portfolio() {
       description: 'Description of This application is a single day planning tool. The code writted using HTML, CSS and Bootstrap CSS framework, Javascript jQuery libaries, and font-awesome icon toolkit. The application can be used to record and save 9am-5pm workday.',
       url: 'https://github.com/KittiyaLooknam/workday-sceduler.git'
     },
+    {
+      id: 7,
+      image: Project7Img,
+      title: 'Quiz Game Challenge',
+      description: 'Description of This is a Quiz game. The code writted using HTML, CSS and Bootstrap CSS framework, Javascript jQuery libaries, and font-awesome icon toolkit.',
+      url: 'https://github.com/KittiyaLooknam/Quiz-game.git'
+    },
+    {
+      id: 8,
+      image: Project8Img,
+      title: 'Employee-Tracker',
+      description: 'Description of This application is a command-line based content management system which can be used to manage employee data. It is written using JavaScript and MySQL and should be run using the Node.js JavaScript runtime environment. The node package manager inquirer is used to provide the interface for the application and to prompt the user for inputs.',
+      url: 'https://github.com/KittiyaLooknam/Employee-Tracker.git'
+    },
+    {
+      id: 9,
+      image: Project9Img,
+      title: 'Password-Generator',
+      description: 'Description of This is the password generator for the user-defined length between 8-128 characters selected character types (uppercase, lowercase, numeric, and special). The password uses HTML to create the button to initiate password generation and prompts and confirms to select password parameters. After selection, the generated password is displayed in the browser window.',
+      url: 'https://github.com/KittiyaLooknam/Password-Generator.git'
+    },
+    {
+      id: 8,
+      image: Project8Img,
+      title: 'Employee-Tracker',
+      description: 'Description of This is a Quiz game. The code writted using HTML, CSS and Bootstrap CSS framework, Javascript jQuery libaries, and font-awesome icon toolkit.',
+      url: 'https://github.com/KittiyaLooknam/Employee-Tracker.git'
+    },
+    {
+      id: 8,
+      image: Project8Img,
+      title: 'Employee-Tracker',
+      description: 'Description of This is a Quiz game. The code writted using HTML, CSS and Bootstrap CSS framework, Javascript jQuery libaries, and font-awesome icon toolkit.',
+      url: 'https://github.com/KittiyaLooknam/Employee-Tracker.git'
+    },
+    {
+      id: 8,
+      image: Project8Img,
+      title: 'Employee-Tracker',
+      description: 'Description of This is a Quiz game. The code writted using HTML, CSS and Bootstrap CSS framework, Javascript jQuery libaries, and font-awesome icon toolkit.',
+      url: 'https://github.com/KittiyaLooknam/Employee-Tracker.git'
+    },
+    
+
+
   ];
 
   const handleClick = (url) => {
@@ -69,7 +118,7 @@ export default function Portfolio() {
             Portfolio
           </FancyText>
       
-      <div className="project-grid">
+      <div className="grid-container">
         {projects.map(project => (
           <div key={project.id} className="project-card">
             <a href={project.url} target="_blank" rel="noopener noreferrer">
